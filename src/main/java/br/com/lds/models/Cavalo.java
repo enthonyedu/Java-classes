081dea2d5010aabff3b0d57ec468b9a3119b5dd7
Enthony <enthonyedu@gmail.com>
2022-03-28 19:48:37 -0300

    feat: add pasta models

---
+++ src/main/java/br/com/lds/models/Cavalo.java
@@ -0,0 +1,11 @@
+package br.com.lds.models;
+
+public class Cavalo extends Animal {
+	public Cavalo(int id, String nome) {
+
+		setId(id);
+		setNome(nome);
+
+	}
+
+}
---
+++ src/main/java/br/com/lds/models/Girafa.java
@@ -0,0 +1,9 @@
+package br.com.lds.models;
+
+public class Girafa extends Animal {
+	public Girafa(int id, String nome) {
+		setId(id);
+		setNome(nome);
+	}
+
+}
